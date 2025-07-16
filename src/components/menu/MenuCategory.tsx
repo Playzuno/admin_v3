e58baeaa13cc8357a104677ca438fa4f222214f9
Om Prakash B K L <prakash@playzuno.com>
2025-07-16 12:09:22 +0530

    refactor: changed the colors mostly in product page

--- src/components/menu/MenuCategory.tsx
+++ src/components/menu/MenuCategory.tsx
@@ -104,11 +104,11 @@ const MenuCategory: React.FC<MenuCategoryProps> = ({
   };
 
   return (
-    <div className="bg-primary-50 rounded-2xl p-6 w-[330px]">
+    <div className="bg-neutral-100 rounded-2xl p-6 w-[330px]">
       <div className="flex justify-between items-center">
         <h2 className="title-2">{title}</h2>
         <Button
-          variant="zuno-orange-light"
+          variant="zuno-dark"
           onClick={onEdit}
           style={{ padding: '8px 16px' }}
         >
@@ -117,7 +117,7 @@ const MenuCategory: React.FC<MenuCategoryProps> = ({
         </Button>
       </div>
 
-      <div className="border-b border-primary-500 h-2 mb-6"></div>
+      <div className="border-b border-secondary-500 h-2 mb-6"></div>
 
       <div className="space-y-4">
         {items.map((item, index) => (
